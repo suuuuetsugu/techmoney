698cf058d3db65524e103250f822e1347f6de8f6
suuuuetsugu <suuuuetsugu@gmail.com>
2022-09-02 09:29:22 +0900

    create API and check page

--- backend/routes/sunabar.js
+++ backend/routes/sunabar.js
@@ -3,35 +3,9 @@ const router = express.Router();
 const request = require('request')
 const logger = require("../log/winston-setup");
 
-// router.post('/', function(req, res, next) {
-//     logger.info('Info sunabar start');
-
-//     var options = {
-//       "method": "POST",
-//       "url": "https://api.sunabar.gmo-aozora.com/personal/v1/transfer/spaccounts-transfer",
-//       "headers": {
-//         "Accept": "application/json;charset=UTF-8",
-//         "Content-Type": "application/json;charset=UTF-8",
-//         "x-access-token": "YjMwZjAzZjg3M2RhNTkzMTBiMWUwZTZl"
-//       },
-//       body: {
-//         "depositSpAccountId":"SP50220278928",
-//         "debitSpAccountId":"SP30210005043",
-//         "currencyCode":"JPY",
-//         "paymentAmount":"5000",
-//       },
-//       json: true,
-//     };
-//     res.render(options)
-
-//     logger.info('Info sunabar end');
-// });
-
 router.post('/', async(req, res, next) => {
     logger.info('Info sunabar start');
     console.log('body', req.body);
-    
-    const json = req.body;
 
     var options = {
       "method": "POST",
@@ -41,13 +15,7 @@ router.post('/', async(req, res, next) => {
         "Content-Type": "application/json;charset=UTF-8",
         "x-access-token": "YjMwZjAzZjg3M2RhNTkzMTBiMWUwZTZl"
       },
-      body: {
-        "depositSpAccountId":"SP50220278928",
-        "debitSpAccountId":"SP30210005043",
-        "currencyCode":"JPY",
-        "paymentAmount":"3000", // クリックしたボタンに紐づく金額を取得する
-      },
-    //   body: json,
+      body: req.body,
       json: true
     };
 
